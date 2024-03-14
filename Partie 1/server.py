b5b0021fafe377812090c839e98e4c27ff0592ef
JasonGagnard <jas.gagnard@gmail.com>
2024-03-14 16:33:07 +0100

    modif server.py pour limiter accès

--- Partie 1/server.py
+++ Partie 1/server.py
@@ -8,8 +8,17 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 
 clients = []
+MAX_CLIENTS = 2  # Maximum number of clients allowed in a conversation
 
 def handle_client(client_socket, client_address):
+    global clients
+    
+    if len(clients) >= MAX_CLIENTS:
+        print(f"[NEW CONNECTION] {client_address} N'a pas accès le nombre de participant maximum a été atteint")
+        client_socket.send("Server: Le nombre maximum de clients a été atteint.".encode("utf-8"))
+        client_socket.close()
+        return
+    
     print(f"[NEW CONNECTION] {client_address} connected.")
     clients.append((client_socket, client_address))
     
@@ -22,18 +31,22 @@ def handle_client(client_socket, client_address):
             msg = client_socket.recv(1024).decode("utf-8")
             if msg:
                 print(f"[{client_address}] {msg}")
-                send_to_other_clients(client_socket, msg)
+                send_to_other_client(client_socket, msg)
         except ConnectionResetError:
             print(f"Client {client_address} disconnected.")
             break
 
-def send_to_other_clients(sender_socket, message):
-    for client_socket, _ in clients:
-        if client_socket != sender_socket:
-            try:
-                client_socket.send(message.encode("utf-8"))
-            except ConnectionResetError:
-                print("Error sending message to client.")
+    clients.remove((client_socket, client_address))
+    print(f"Conversation ended for {client_address}.")
+    client_socket.close()
+
+def send_to_other_client(sender_socket, message):
+    if len(clients) == 2:
+        receiver_socket, _ = next((sock, addr) for sock, addr in clients if sock != sender_socket)
+        try:
+            receiver_socket.send(message.encode("utf-8"))
+        except ConnectionResetError:
+            print("Error sending message to client.")
 
 def start():
     server_socket.listen()
